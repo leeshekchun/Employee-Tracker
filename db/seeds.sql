b261ef9794d2796e36f84e8aee78a9ab8a76000b
ShekChun Lee <leeshekchun@gmail.com>
2022-06-12 23:08:39 -0600

    add prompt

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,17 @@
-INSERT INTO departments
-    (name)
-VALUES
-    (),
-    (),
-    ();
+INSERT INTO departments (name)
+VALUES  ('Sales'),
+        ('Engineering'),
+        ('Finance');
+        ('Legal')
 
-INSERT INTO roles
-    (title, salary, department_id)
-VALUES
-    ()
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('Salesperson', 8000, 1),
+        ('Lead Engineer', 150000, 2),
+        ('Software Engineer', 120000, 2),
+        ('Account Manager', 160000, 3),
+        ('Accountant', 125000, 3),
+        ('Legal Team Lead', 250000, 4),
+        ('Lawyer', 190000, 4)
 
-INSERT INTO employees
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+VALUES  ('')
--- server.js
+++ server.js
@@ -2,3 +2,29 @@ const db = require('./db/connection');
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "path",
+            message: "What would you like to do?",
+            choices: [
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "Update Employee Role",
+                "Finish"
+            ]  
+        }
+    ])
+    .then((answers) => {
+        console.log(answers)
+    })
+    .catch((error) =>{
+        if (error.isTtyError) {
+            console.error('error occurs!')
+        } else {
+            console.log('successfully selected!')
+        }
+    })
